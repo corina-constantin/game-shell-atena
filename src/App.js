d9088fd1cf43b03f7eaa8dabe3bcf8b848ad5996
corina-constantin <corina.pescaru@gmail.com>
2023-02-20 13:27:28 +0200

    setup game shell

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users").then((response) => {
+      return response.json();
+    }).then((data)=>{
+      setUsers(data)
+    })
+  }, [setUsers]);
+
   return (
     <div className="App">
+      {JSON.stringify(users)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
