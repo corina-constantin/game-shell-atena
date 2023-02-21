4fe46e8f2d7c6792ca1f131a98664ed80161da48
corina-constantin <corina.pescaru@gmail.com>
2023-02-21 16:32:58 +0200

    glitch

--- src/api/users.js
+++ src/api/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://cottony-crystalline-poet.glitch.me/';
 
 export const client = axios.create({
   baseURL: baseUrl,
